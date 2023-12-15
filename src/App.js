8e647cfafb163ffb16936e23b98dba1ead64658e
Chris Braithwaite <chris.braithwaite@entelect.co.uk>
2023-12-15 09:34:16 +0000

    Initial check in

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+
+// Import everything needed to use the `useQuery` hook
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: "https://71z1g.sse.codesandbox.io/",
+  cache: new InMemoryCache(),
+});
+
+
+const GET_DOGS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
+
+const GET_DOG_PHOTO = gql`
+  query Dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+export default function App() {
+  const [selectedDog, setSelectedDog] = useState(null);
+
+  function onDogSelected({ target }) {
+    setSelectedDog(target.value);
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Building Query components 🚀</h2>
+        {selectedDog && <DogPhoto breed={selectedDog} />}
+        <Dogs onDogSelected={onDogSelected} />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
+}
+
+function Dogs({ onDogSelected }) {
+  const { loading, error, data } = useQuery(GET_DOGS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <select name='dog' onChange={onDogSelected}>
+      {data.dogs.map((dog) => (
+        <option key={dog.id} value={dog.breed}>
+          {dog.breed}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+function DogPhoto({ breed }) {
+  const { loading, error, data } = useQuery(GET_DOG_PHOTO, {
+    variables: { breed },
+  });
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+
+  return (
+    <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+  );
+}
